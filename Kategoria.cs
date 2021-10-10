e4c5068d795c38f4153c7e60db89bdb8b5eeb8b9
feljey <85069149+feljey@users.noreply.github.com>
2021-10-10 20:29:57 +0200

    Add files via upload

---
+++ Kategoria.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace infoinfo.Models;
+public class Kategoria
+{
+    [Key] // zbędne jest po nazwie ID
+    [Display(Name="identyfikator kategorii")]  
+    public int Kategorii { get; set; }
+    
+    public string NazwaKategorii {  get; set; }
+    public string OpisKategorii {  get; set; }   
+    public string Ikona { get; set; }
+    public bool Aktywna {  get; set; }
+    public int Informacja { get; set; }
+
+}
